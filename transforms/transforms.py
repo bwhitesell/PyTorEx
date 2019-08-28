34c6e4b62aa2a493ff6207885b39f2d8f0d1b368
Ben Whitesell <whitesell.ben@gmail.com>
2019-08-28 12:47:59 -0700

    added get word2vec weights

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -1,3 +1,12 @@
+import gensim
+import torch
+
+
 class MakeRGB:
     _order = 0
     def __call__(self, item): return item.convert('RGB')
+
+
+def get_word_2_vec_weights():
+    model = gensim.models.KeyedVectors.load_word2vec_format('path/to/file')
+    return torch.FloatTensor(model.vectors)
